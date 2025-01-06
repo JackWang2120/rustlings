52a3eb6ea07629119e2caef217ad25939061a950
JackWang2120 <JackWang2120@gmail.com>
2025-01-06 10:32:02 +0800

    rustlings:finish:00-03

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,5 +1,5 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+const NUMBER:i32 = 3;//在 Rust 中，const 常量必须显式指定类型。这是因为常量在编译时就需要确定其类型和值，以确保代码的类型安全和性能优化。
 
 fn main() {
     println!("Number: {NUMBER}");
